ca183c9e4274416b3df71b828815942d258efe27
rjwx60 <614054722@qq.com>
2020-03-14 21:59:49 +0800

    add: add juejin suport

--- js/background.js
+++ js/background.js
@@ -1,5 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('request: ', request);
   console.log('sender: ', sender);
-  sendResponse("mp.weixin.qq.com" === request.location.hostname);
+  const Wx = "mp.weixin.qq.com" === request.location.hostname;
+  const Juejin = "juejin.im" === request.location.hostname;
+  const response = Wx ? 'wx' : Juejin ? 'juejin' : '';
+  sendResponse(response);
 });
--- js/content-script.js
+++ js/content-script.js
@@ -8,12 +8,38 @@ chrome.runtime.sendMessage({
 }, (response) => {
   if (response) {
     window.onload = function() {
-      const QRcode = document.querySelector('.qr_code_pc');
-      console.log('QRcode: ', QRcode);
-      const Container = document.querySelector('.rich_media_area_primary_inner');
-      console.log('Container: ', Container);
-      QRcode.style = 'display:none;';
-      Container.style  = 'max-width: 1377px;'
+      switch(response) {
+        case 'wx':
+          const QRcode = document.querySelector('.qr_code_pc');
+          // console.log('QRcode: ', QRcode);
+          const Container = document.querySelector('.rich_media_area_primary_inner');
+          // console.log('Container: ', Container);
+          QRcode.style = 'display:none;';
+          Container.style  = 'max-width: 1377px;'
+          break;
+        case 'juejin':
+          const MainContainer = document.querySelector('main.container');
+          MainContainer.style.maxWidth = '1260px';
+          const MainArea = document.querySelector('.main-area');
+          MainArea.style.width = '1200px';
+          const InfoBar = document.querySelector('.article-suspended-panel'); 
+          InfoBar.style.right = '30px';
+          const SideBar = document.querySelector('.sidebar');
+          SideBar.style.left = '10px';
+          SideBar.style.position = 'fixed';
+          const SideBarList = document.querySelectorAll('.sidebar-block');
+          SideBarList.forEach((cv, index, nodeList) => {
+            if(!(/catalog-block.pure/.test(cv.className))) {
+              cv.style.display = 'none';
+            }
+          });
+          document.querySelectorAll('p').forEach(cv => {cv.style.margin = '0px'})
+          document.querySelectorAll('h3').forEach(cv => {cv.style.margin = '0px'})
+          document.querySelectorAll('h4').forEach(cv => {cv.style.margin = '0px'})
+          break;
+        default:
+          break;
+      }
     }
   }
 });

d3f86ff9e7d4dd2fa052d34b702619b10f5b2884
rjwx60 <614054722@qq.com>
2020-03-17 10:25:23 +0800

    change: use vue failed

--- js/popup.js
+++ js/popup.js
@@ -9,30 +9,67 @@ function sendMessage(message) {
         if (chrome.runtime.lastError) {
           console.log("error:" + chrome.runtime.lastError);
         } else {
-          // console.log("response: ", response);
-          // console.log("message successfully sent");
-          init(response.styleRules, response.scriptRules);
+          if (response) {
+            init(response.styleRules, response.scriptRules);
+          }
         }
       });
     }
   );
 }
 
-sendMessage({ cmd: "popup" });
-
 
 function init(styleRules, scriptRules) {
   new Vue({
     el: '#list',
+    template: `
+    <ul class="rules" v-for="(item, listIndex) in list" :key="listIndex">
+    <h3 class="ruleTitle">
+      <span>当前域名：{{item.hostname}}</span>
+      <div class="statusSwitch" v-if="item.on">
+        <input class="switch" type="radio" :name="'label' + item.id" :id="'label' + item.id" v-model="item.on"  @click="changeStatus(item)">
+        <label :for="'label' + item.id"></label>
+      </div>
+    </h3>
+    <ul class="ruleList" v-if="item.styleRules.length">
+      <p>Style规则：</p>
+      <li class="ruleItem" v-for="(styleRules, index) in item.styleRules" :key="index"> 
+        <ul class="valueList">
+          <li class="vltitle">{{styleRules.target}}</li>
+          <div class="vlvalue">
+            <li v-for="valueItem in styleRules.valueArr">{{valueItem.key}}:{{valueItem.value}}</li>
+          </div>
+        </ul>
+        <div class="valueSwitch">
+          <input class="switch" type="radio" :name="'label' + index" :id="'label' + index" v-model="styleRules.on" @click="changeStatus(styleRules)">
+          <label :for="'label' + index"></label>
+        </div>
+      </li>
+    </ul>
+    <ul class="ruleList" v-if="item.scriptRules.length">
+      <p>Script规则：</p>
+      <li class="ruleItem" v-for="(scriptRules, index) in item.scriptRules" :key="index"> 
+        <ul class="valueList">
+          <li class="vltitle">{{scriptRules.target}}</li>
+          <div class="vlvalue">
+            <li v-for="valueItem in scriptRules.valueArr">{{valueItem.key}}:{{valueItem.value}}</li>
+          </div>
+        </ul>
+        <div class="valueSwitch">
+          <input class="switch" type="radio" :name="'label' + index" :id="'label' + index" v-model="scriptRules.on" @click="changeStatus(scriptRules)">
+          <label :for="'label' + index"></label>
+        </div>
+      </li>
+    </ul>`,
     data: {
       styleRules: styleRules,
       scriptRules: scriptRules,
-      visible: false,
-      blogTitle: 'hi'
     },
     data() {
       return {
+        hasChange: false,
         list:[{
+          id: 123,
           hostname: 'mp.weixin.qq.com',
           urlparams: '',
           on: true,
@@ -69,14 +106,19 @@ function init(styleRules, scriptRules) {
       }
     },
     methods: {
-      show: function () {
-        this.visible = true;
+      changeStatus(rule){
+        this.hasChange = true;
+        rule.on = rule.on ? false : true;
+      },
+      refreshStatus(){
+        console.log('存储并刷新页面')
+      },
+      addRules(){
+        console.log('跳转 background 页')
       }
-    },
-    // render: function (createElement) {
-    //   return createElement('h1', this.blogTitle)
-    // }
+    }
   });
 }
 
 
+sendMessage({ cmd: "popup" });

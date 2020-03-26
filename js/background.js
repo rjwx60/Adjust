7b8ad07b32db9a3a4cc2ffbd0390009c4fc20613
rjwx60 <614054722@qq.com>
2020-03-26 16:19:04 +0800

    ..

--- js/background.js
+++ js/background.js
@@ -1,252 +1,280 @@
 const ruleArrayTest = [
   {
-    hostname: 'mp.weixin.qq.com',
-    urlparams: '',
+    hostname: "mp.weixin.qq.com",
+    urlparams: "",
     on: true,
     id: 12323,
     scriptRules: [],
     styleRules: [
       {
+        on: true,
         only: true,
-        target: '.qr_code_pc',
-        excepetTarget: '',
+        target: ".qr_code_pc",
+        excepetTarget: "",
         id: 123141555,
         valueArr: [
           {
             id: 1123,
-            key: 'display',
-            value: 'none',
+            key: "display",
+            value: "none",
+            combine: "display:none;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: true,
-        target: '.rich_media_area_primary_inner',
-        excepetTarget: '',
+        target: ".rich_media_area_primary_inner",
+        excepetTarget: "",
         id: 123141553,
         valueArr: [
           {
             id: 2141,
-            key: 'maxWidth',
-            value: '1377px',
+            key: "maxWidth",
+            value: "1377px",
+            combine: "maxWidth:1377px;"
           }
-        ],
-        on: true
+        ]
       }
     ]
   },
   {
-    hostname: 'juejin.im',
-    urlparams: '',
+    hostname: "juejin.im",
+    urlparams: "",
     on: true,
     id: 12323232899,
     scriptRules: [],
     styleRules: [
       {
+        on: true,
         only: true,
-        target: 'main.container',
-        excepetTarget: '',
+        target: "main.container",
+        excepetTarget: "",
         id: 1231415211,
         valueArr: [
           {
             id: 112153,
-            key: 'maxWidth',
-            value: '1260px',
+            key: "maxWidth",
+            value: "1260px",
+            combine: "maxWidth:1260px;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: true,
-        target: '.main-area',
-        excepetTarget: '',
+        target: ".main-area",
+        excepetTarget: "",
         id: 12314152119,
         valueArr: [
           {
             id: 221124,
-            key: 'width',
-            value: '1200px',
+            key: "width",
+            value: "1200px",
+            combine: "width:1200px;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: true,
-        target: '.article-suspended-panel',
-        excepetTarget: '',
+        target: ".article-suspended-panel",
+        excepetTarget: "",
         id: 123141521993,
         valueArr: [
           {
             id: 323131,
-            key: 'right',
-            value: '30px',
+            key: "right",
+            value: "30px",
+            combine: "right:30px;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: true,
-        target: '.sidebar',
-        excepetTarget: '',
+        target: ".sidebar",
+        excepetTarget: "",
         id: 12314152167233,
         valueArr: [
           {
             id: 4232,
-            key: 'left',
-            value: '10px',
+            key: "left",
+            value: "10px",
+            combine: "left:10px;"
           },
           {
             id: 54231,
-            key: 'position',
-            value: 'fixed'
+            key: "position",
+            value: "fixed",
+            combine: "position:fixed;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: false,
-        target: '.sidebar-block',
-        excepetTarget: 'catalog-block.pure',
+        target: ".sidebar-block",
+        excepetTarget: "catalog-block.pure",
         id: 12314152143245,
         valueArr: [
           {
             id: 613123,
-            key: 'display',
-            value: 'none',
+            key: "display",
+            value: "none",
+            combine: "display:none"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: false,
-        target: 'p',
-        excepetTarget: '',
+        target: "p",
+        excepetTarget: "",
         id: 1231415211013,
         valueArr: [
           {
             id: 721351,
-            key: 'margin',
-            value: '0px',
+            key: "margin",
+            value: "0px",
+            combine: "margin:0px;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: false,
-        target: 'h3',
-        excepetTarget: '',
+        target: "h3",
+        excepetTarget: "",
         id: 1231415211032,
         valueArr: [
           {
             id: 8123,
-            key: 'margin',
-            value: '0px',
+            key: "margin",
+            value: "0px",
+            combine: "margin:0px;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: false,
-        target: 'h4',
-        excepetTarget: '',
+        target: "h4",
+        excepetTarget: "",
         id: 123141521191231,
         valueArr: [
           {
             id: 9123,
-            key: 'margin',
-            value: '0px',
+            key: "margin",
+            value: "0px",
+            combine: "margin:0px;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: false,
-        target: 'ol',
-        excepetTarget: '',
+        target: "ol",
+        excepetTarget: "",
         id: 123141521211419,
         valueArr: [
           {
             id: 10123,
-            key: 'margin',
-            value: '0px',
+            key: "margin",
+            value: "0px",
+            combine: "margin:0px;"
           }
-        ],
-        on: true
+        ]
       },
       {
+        on: true,
         only: false,
-        target: 'pre',
-        excepetTarget: '',
+        target: "pre",
+        excepetTarget: "",
         id: 1231231012,
         valueArr: [
           {
             id: 1124232,
-            key: 'margin',
-            value: '0px',
+            key: "margin",
+            value: "0px",
+            combine: "margin:0px;"
           }
-        ],
-        on: true
+        ]
       }
     ]
   },
   {
-    hostname: 'github.com',
-    urlparams: '',
+    hostname: "github.com",
+    urlparams: "",
     on: true,
     id: 12323232812399,
     scriptRules: [],
     styleRules: [
       {
+        on: true,
         only: true,
-        target: '.col-md-9',
-        excepetTarget: '',
+        target: ".col-md-9",
+        excepetTarget: "",
         id: 123141521114991,
         valueArr: [
           {
             id: 1121598993,
-            key: 'width',
-            value: '100%',
+            key: "width",
+            value: "100%",
+            combine: "width:100%;"
           }
-        ],
-        on: true
+        ]
       }
     ]
   }
-] 
+];
 
-
-var ruleArray = null;
+var globalRuleCache = null;
 
 // 监听页面变化
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  getStorage().then(res => {
-    // set ruleArray
-    ruleArray = res;
-    return new Promise((resolve, reject) => {
-      const target = res && res.filter(cv => {
-        return cv.hostname === request.location.hostname;
-      })[0];
-      target ? resolve(target) : reject('no match target');
+  getStorage()
+    .then(res => {
+      // set ruleArray
+      ruleArray = res;
+      return new Promise((resolve, reject) => {
+        const target =
+          res &&
+          res.filter(cv => {
+            return cv.hostname === request.location.hostname;
+          })[0];
+        target ? resolve(target) : reject("no match target");
+      });
     })
-  }).then(target => {
-    return new Promise((resolve, reject) => {
-      sendResponse(target);
+    .then(target => {
+      return new Promise((resolve, reject) => {
+        sendResponse(target);
+      });
     })
-  }).catch(err => {
-    console.log('error: ', err);
-  });
+    .catch(err => {
+      console.log("error: ", err);
+    });
   return true;
 });
 
+// 主逻辑
+function init() {
+  getStorage().then(result => {
+    globalRuleCache = result;
+    ruleListRender(ruleArrayTest);
+  });
+}
+
 // 获取存储
-function getStorage(target = 'rules'){
+function getStorage(target = "rules") {
   // 读取数据，第一个参数是指定要读取的key以及设置默认值
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get(target, function(items) {
-      resolve(items.rules);
+      resolve(ruleArrayTest);
+      console.log("ruleArrayTest: ", ruleArrayTest);
+      // resolve(items.rules);
     });
-  })
+  });
 }
 
 // 设置存储
@@ -254,35 +282,60 @@ function setStorage(target) {
   // 保存数据
   return new Promise((resolve, reject) => {
     if (target) {
-      const index = ruleArray.findIndex(cv => cv.hostname == target.hostname);
+      const index = globalRuleCache.findIndex(
+        cv => cv.hostname == target.hostname
+      );
       if (index !== -1) {
-        ruleArray.splice(index, 1, target);
+        globalRuleCache.splice(index, 1, target);
         // e.g. {color: 'blue'}
-        chrome.storage.sync.set({rules: ruleArray}, function() {
+        chrome.storage.sync.set({ rules: globalRuleCache }, function() {
           resolve({
             code: 200,
             error: 0,
-            info: 'save success',
+            info: "save success"
           });
         });
       }
     }
-  })
+  });
 }
 
-function init(rulesList) {
-  if (rulesList) {
-    console.log('rulesList: ', rulesList);
+// 生成随机数
+function randomCode(len = 16, isMix = false) {
+  /**** 去掉了容易混淆的字符oOLl,Vv,Uu,I ****/
+  const cahrs = isMix ? "ABCDEFGHJKMNPQRSTWXYZabcdefghijkmnpqrstwxyz123456789" : '123456789'
+  const maxPos = cahrs.length;
+  let randomString = "";
+  for (i = 0; i < len; i++) {
+    randomString += cahrs.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return randomString;
+}
+
+// 列表渲染
+function ruleListRender(rulesList) {
+  if (rulesList && rulesList.length) {
     new Vue({
-      el: '#app',
+      el: "#app",
       data: {
         rulesList
+      },
+      methods: {
+        changeStyle(rule, styleRules, valueItem) {
+          console.log("rule: ", rule);
+          console.log("styleRules: ", styleRules);
+          console.log("valueItem: ", valueItem);
+        },
+        toggleRule(status) {
+          status.on = !status.on;
+        },
+        deleteOne() {
+          console.log(randomCode());
+        },
+        addOne() {}
       }
-    })
+    });
   }
 }
 
-init(ruleArrayTest);
-
-
-// chrome-extension://lnhfhfemehnokhdpeagckiomalhgibfg/background.html
+init();
